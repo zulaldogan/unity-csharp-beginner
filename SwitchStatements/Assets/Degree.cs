aa7569bf6eeede4f314ea1b5011ff6d783289a18
ZÜLAL DOĞAN <zulaldogan@users.noreply.github.com>
2017-08-12 16:33:32 +0300

    SwitchStatements
    https://www.youtube.com/watch?v=06KsyWwNoM0&index=4&list=PLadYLGMfR6Lphpq1PZFo25_acWAEuoLh_

---
+++ SwitchStatements/Assets/Degree.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Degree : MonoBehaviour {
+    public int selectedLevel;
+    private int easy = 0, medium = 1, hard = 2;
+
+	// Use this for initialization
+	void Start () {
+		switch(selectedLevel)
+        {
+            case 0:
+                Debug.Log("You selected Easy");
+                break;
+            case 1:
+                Debug.Log("You selected Medium");
+                break;
+            case 2:
+                Debug.Log("You selected Hard");
+                break;
+            default:
+                Debug.Log("You didn't select");
+                break;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ SwitchStatements/Assets/Switch_Statements.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch_Statements : MonoBehaviour
+{
+    public int a ;
+    public int b ;
+    public int c ;
+    public int average;
+
+    // Use this for initialization
+    void Start ()
+    {
+        average = (a + b + c) / 3;
+
+       /* if(average==16)
+        {
+            Debug.Log("Average is 16");
+        }
+        else if(average==5)
+        {
+            Debug.Log("Average is 5");
+        }
+        else if (average ==10)
+        {
+            Debug.Log("Average is 10");
+        }
+        else
+        {
+            Debug.Log("Invalid");
+        } */
+
+        switch (average)
+        {
+            case 16:
+                Debug.Log("Average is 16");
+                break;
+            case 10:
+                Debug.Log("Average is 10");
+                break;
+            default:
+                Debug.Log("This is default case!");
+                break;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
