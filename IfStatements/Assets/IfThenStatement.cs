249b972c0f01c7130902367a4b2adfb125e35214
ZÜLAL DOĞAN <zulaldogan@users.noreply.github.com>
2017-08-12 16:29:56 +0300

    IfStatements
    https://www.youtube.com/watch?v=-VY083F-71c&index=2&list=PLadYLGMfR6Lphpq1PZFo25_acWAEuoLh_

---
+++ IfStatements/Assets/IfThenStatement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfThenStatement : MonoBehaviour {
+
+    public int myAge = 26;
+
+    public bool canDrink = true;
+
+
+    private void Start()
+    {
+        if (myAge > 18)
+        {
+            Debug.Log("I am older than 18");
+        }
+        else if (myAge < 5)
+        {
+            Debug.Log("Less than 5");
+        }
+        else
+        {
+            Debug.Log("Invalid");
+        }
+
+        if (canDrink == true)
+        {
+            Debug.Log("You can drink!");
+        }
+        else
+        {
+            Debug.Log("You can't!");
+        }
+    }
+
+}
