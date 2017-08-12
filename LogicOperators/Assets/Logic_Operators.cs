2ace960b83e5d32ddc37c014eb3a485efcb49ec4
ZÜLAL DOĞAN <zulaldogan@users.noreply.github.com>
2017-08-12 16:31:58 +0300

    LogicOperators
    https://www.youtube.com/watch?v=FROlIXy7RE0&list=PLadYLGMfR6Lphpq1PZFo25_acWAEuoLh_&index=3

---
+++ LogicOperators/Assets/Logic_Operators.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Logic_Operators : MonoBehaviour
+{
+    public int a;
+    public int b;
+    public int c;
+    public int average;
+
+	// Use this for initialization
+	void Start ()
+    {
+        average = (a + b + c)/ 3;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
