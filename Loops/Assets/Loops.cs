d9ac0c2edba2533fcaeadf6f280560d8cc694ef0
ZÜLAL DOĞAN <zulaldogan@users.noreply.github.com>
2017-08-12 16:34:33 +0300

    Loops
    https://www.youtube.com/watch?v=SY1fUFun670&index=5&list=PLadYLGMfR6Lphpq1PZFo25_acWAEuoLh_

---
+++ Loops/Assets/Loops.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loops : MonoBehaviour {
+
+    public int[] numbers = { 5, 6, 7, 8 };
+
+    public int apples = 100;
+	// Use this for initialization
+	void Start ()
+    {
+		for(int i=0;i<numbers.Length;i++)
+        {
+            Debug.Log(numbers[i]);
+        }
+
+        foreach(var number in numbers)
+        {
+            Debug.Log(number);
+        }
+
+        do
+        {
+            Debug.Log("Apples" + apples);
+            apples--;
+        }
+        while (apples >= 1);
+
+        Debug.Log("The wile loop finished! Yay for no crash!,");
+	}
+	
+	
+}
