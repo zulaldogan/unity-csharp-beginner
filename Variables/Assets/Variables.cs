3fef07b0c652db9c4b28062bce09a1284931988d
ZÜLAL DOĞAN <zulaldogan@users.noreply.github.com>
2017-08-12 16:28:27 +0300

    Variables
    https://www.youtube.com/watch?v=apJoi_YFVqk&list=PLadYLGMfR6Lphpq1PZFo25_acWAEuoLh_&index=1

---
+++ Variables/Assets/Variables.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour 
+{
+
+	public string myName = "Unity3D";
+	public float speed = 5.5f;
+	public bool isCool = true;
+	private int myAge = 26;
+
+	public int health = 100;
+	private int maxAmmoCount = 100;
+
+}
