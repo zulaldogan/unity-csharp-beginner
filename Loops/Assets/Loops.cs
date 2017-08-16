9b4d39848789f87680e6ca783e605ab3d448d37a
ZÜLAL DOĞAN <zulaldogan@users.noreply.github.com>
2017-08-17 01:20:15 +0300

    Update Loops.cs

--- Loops/Assets/Loops.cs
+++ Loops/Assets/Loops.cs
@@ -1,21 +1,18 @@
-﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Loops : MonoBehaviour {
-
+public class Loops : MonoBehaviour
+{
     public int[] numbers = { 5, 6, 7, 8 };
-
     public int apples = 100;
-	// Use this for initialization
-	void Start ()
+
+    void Start()
     {
-		for(int i=0;i<numbers.Length;i++)
+        for (int i = 0; i < numbers.Length; i++)
         {
             Debug.Log(numbers[i]);
         }
 
-        foreach(var number in numbers)
+        foreach (var number in numbers)
         {
             Debug.Log(number);
         }
@@ -25,10 +22,8 @@ public class Loops : MonoBehaviour {
             Debug.Log("Apples" + apples);
             apples--;
         }
-        while (apples >= 1);
 
+        while (apples >= 1);
         Debug.Log("The wile loop finished! Yay for no crash!,");
-	}
-	
-	
+    }
 }
