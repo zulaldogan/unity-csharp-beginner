889f0f52ad57a8513dd29f34b795a0043ab286f5
ZÜLAL DOĞAN <zulaldogan@users.noreply.github.com>
2017-08-17 01:27:50 +0300

    Update Switch_Statements.cs

--- SwitchStatements/Assets/Switch_Statements.cs
+++ SwitchStatements/Assets/Switch_Statements.cs
@@ -1,35 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Switch_Statements : MonoBehaviour
 {
-    public int a ;
-    public int b ;
-    public int c ;
+    public int a;
+    public int b;
+    public int c;
     public int average;
 
-    // Use this for initialization
-    void Start ()
+    void Start()
     {
         average = (a + b + c) / 3;
-
-       /* if(average==16)
-        {
-            Debug.Log("Average is 16");
-        }
-        else if(average==5)
-        {
-            Debug.Log("Average is 5");
-        }
-        else if (average ==10)
-        {
-            Debug.Log("Average is 10");
-        }
-        else
-        {
-            Debug.Log("Invalid");
-        } */
 
         switch (average)
         {
@@ -44,9 +24,9 @@ public class Switch_Statements : MonoBehaviour
                 break;
         }
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    void Update()
+    {
+
+    }
 }
