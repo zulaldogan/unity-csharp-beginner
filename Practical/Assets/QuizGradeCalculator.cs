19380e332ebf906145001f971a861235bf5bd1a7
ZÜLAL DOĞAN <zulaldogan@users.noreply.github.com>
2017-08-16 23:26:30 +0300

    Update QuizGradeCalculator.cs

--- Practical/Assets/QuizGradeCalculator.cs
+++ Practical/Assets/QuizGradeCalculator.cs
@@ -4,14 +4,19 @@ public class QuizGradeCalculator : MonoBehaviour
 {
     public float quiz1, quiz2, quiz3, quiz4, quiz5;
     public float average;
-
+    
+    public float Rndm()
+    {
+        return Random.Range(0f, 100.1f);//Random Float Numbers(0-100)
+    }
+    
     void Start()
     {
-        quiz1 = Random.Range(0f, 100.1f);//Random Float Numbers(0-100)
-        quiz2 = Random.Range(0f, 100.1f);
-        quiz3 = Random.Range(0f, 100.1f);
-        quiz4 = Random.Range(0f, 100.1f);
-        quiz5 = Random.Range(0f, 100.1f);
+        quiz1 = Rndm();
+        quiz2 = Rndm();
+        quiz3 = Rndm();
+        quiz4 = Rndm();
+        quiz5 = Rndm();
 
         average = (quiz1 + quiz2 + quiz3 + quiz4 + quiz5) / 5;
         average = Mathf.Round(average * 100) / 100;
