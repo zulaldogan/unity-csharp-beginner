aaae64f0ada3ab65e26c42090077eedb2d98019c
ZÜLAL DOĞAN <zulaldogan@users.noreply.github.com>
2017-08-17 01:28:47 +0300

    Update Degree.cs

--- SwitchStatements/Assets/Degree.cs
+++ SwitchStatements/Assets/Degree.cs
@@ -1,14 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Degree : MonoBehaviour {
+public class Degree : MonoBehaviour
+{
     public int selectedLevel;
     private int easy = 0, medium = 1, hard = 2;
 
-	// Use this for initialization
-	void Start () {
-		switch(selectedLevel)
+    void Start()
+    {
+        switch (selectedLevel)
         {
             case 0:
                 Debug.Log("You selected Easy");
@@ -20,13 +19,13 @@ public class Degree : MonoBehaviour {
                 Debug.Log("You selected Hard");
                 break;
             default:
-                Debug.Log("You didn't select");
+                Debug.Log("You didn't select!");
                 break;
         }
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
+
+    void Update()
+    {
+
+    }
 }
